70d5ca014cbd8cd356192e0dad003835dfea575f
Lorena Borges <lobcborges@gmail.com>
2022-04-14 14:07:07 -0300

    modificações e finalização

--- AprendendoExceções/ContaCorrente.cs
+++ AprendendoExceções/ContaCorrente.cs
@@ -1,33 +1,26 @@
-﻿namespace AprendendoExceções
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AprendendoExceções
 {
     public class ContaCorrente
     {
-        public Cliente Titular { get; set; }
+        private static int TaxaOperacao;
 
         public static int TotalDeContasCriadas { get; private set; }
 
+        public Cliente Titular { get; set; }
 
-        private int _agencia;
-        public int Agencia
-        {
-            get
-            {
-                return _agencia;
-            }
-            set
-            {
-                if(value <= 0)
-                {
-                    return;
-                }
+        public int ContadorSaquesNaoPermitidos { get; private set; }
+        public int ContadorTransferenciasNaoPermitidas { get; private set; }
 
-                _agencia = value;
-            }
-        }
-        public int Numero { get; set; }
-        
+        public int Numero { get; }
+        public int Agencia { get; }
+
         private double _saldo = 100;
-
         public double Saldo
         {
             get
@@ -45,25 +38,39 @@
             }
         }
 
-
         public ContaCorrente(int agencia, int numero)
         {
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O argumento agencia deve ser maior que 0.", nameof(agencia));
+            }
+
+            if (numero <= 0)
+            {
+                throw new ArgumentException("O argumento numero deve ser maior que 0.", nameof(numero));
+            }
+
             Agencia = agencia;
             Numero = numero;
 
             TotalDeContasCriadas++;
+            TaxaOperacao = 30 / TotalDeContasCriadas;
         }
 
-
-        public bool Sacar(double valor)
+        public void Sacar(double valor)
         {
+            if (valor < 0)
+            {
+                throw new ArgumentException("Valor inválido para o saque.", nameof(valor));
+            }
+
             if (_saldo < valor)
             {
-                return false;
+                ContadorSaquesNaoPermitidos++;
+                throw new SaldoInsuficienteException(Saldo, valor);
             }
 
             _saldo -= valor;
-            return true;
         }
 
         public void Depositar(double valor)
@@ -71,17 +78,24 @@
             _saldo += valor;
         }
 
-
-        public bool Transferir(double valor, ContaCorrente contaDestino)
+        public void Transferir(double valor, ContaCorrente contaDestino)
         {
-            if (_saldo < valor)
+            if (valor < 0)
             {
-                return false;
+                throw new ArgumentException("Valor inválido para a transferência.", nameof(valor));
             }
 
-            _saldo -= valor;
+            try
+            {
+                Sacar(valor);
+            }
+            catch (SaldoInsuficienteException ex)
+            {
+                ContadorTransferenciasNaoPermitidas++;
+                throw new OperacaoFinanceiraException("Operação não realizada.", ex);
+            }
+
             contaDestino.Depositar(valor);
-            return true;
         }
     }
 }
--- AprendendoExceções/Program.cs
+++ AprendendoExceções/Program.cs
@@ -6,49 +6,102 @@ using System.Threading.Tasks;
 
 namespace AprendendoExceções
 {
-    public class Program
+    class Program
     {
         static void Main(string[] args)
         {
             try
             {
-                Metodo();
+                CarregarContas();
             }
-            catch (DivideByZeroException excecao)
+            catch (Exception)
             {
-                Console.WriteLine("Não é possível divisão por 0");
-            }
-            catch (Exception excecao)
-            {
-                Console.WriteLine(excecao.Message);
-                Console.WriteLine(excecao.StackTrace);
+                Console.WriteLine("CATCH NO METODO MAIN");
             }
 
+            Console.WriteLine("Execução finalizada. Tecle enter para sair");
             Console.ReadLine();
         }
 
-        static int Dividir(int numero, int divisor)
+        private static void CarregarContas()
+        {
+            using (LeitorDeArquivo leitor = new LeitorDeArquivo("teste.txt"))
+            {
+                leitor.LerProximaLinha();
+            }
+
+
+
+            // ---------------------------------------------
+
+            //LeitorDeArquivo leitor = null;
+            //try
+            //{
+            //    leitor = new LeitorDeArquivo("contasl.txt");
+
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //    leitor.LerProximaLinha();
+            //}
+            //finally
+            //{
+            //    Console.WriteLine("Executando o finally");
+            //    if(leitor != null)
+            //    {
+            //        leitor.Fechar();
+            //    }
+            //}
+        }
+
+        private static void TestaInnerException()
+        {
+            try
+            {
+                ContaCorrente conta1 = new ContaCorrente(4564, 789684);
+                ContaCorrente conta2 = new ContaCorrente(7891, 456794);
+
+                // conta1.Transferir(10000, conta2);
+                conta1.Sacar(10000);
+            }
+            catch (OperacaoFinanceiraException e)
+            {
+                Console.WriteLine(e.Message);
+                Console.WriteLine(e.StackTrace);
+
+                // Console.WriteLine("Informações da INNER EXCEPTION (exceção interna):");
+
+            }
+        }
+
+        // Teste com a cadeia de chamada:
+        // Metodo -> TestaDivisao -> Dividir
+        private static void Metodo()
+        {
+            TestaDivisao(0);
+        }
+
+        private static void TestaDivisao(int divisor)
+        {
+            int resultado = Dividir(10, divisor);
+            Console.WriteLine("Resultado da divisão de 10 por " + divisor + " é " + resultado);
+        }
+
+        private static int Dividir(int numero, int divisor)
         {
             try
             {
                 return numero / divisor;
             }
-            catch (Exception)
+            catch (DivideByZeroException)
             {
-                Console.WriteLine("Exceção com numero= " + numero + " e divisor= " + divisor);
+                Console.WriteLine("Exceção com numero=" + numero + " e divisor=" + divisor);
                 throw;
+                Console.WriteLine("Código depois do throw");
             }
         }
 
-        static void Metodo()
-        {
-            TestaDivisao(0);
+        // numero = 1
+        // divisor = 2;
 
-        }
-
-        static void TestaDivisao(int divisor)
-        {
-            Dividir(10, divisor);
-        }
     }
 }
